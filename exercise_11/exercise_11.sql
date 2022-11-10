3efd8ee44f97196da5058ed83d30129f811d642c
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-11-10 10:49:47 -0800

    eleventh test done

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,16 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+SELECT 
+	film.title,
+    count(*)
+FROM
+	film
+    LEFT JOIN inventory on inventory.film_id = film.film_id
+    LEFT JOIN rental on rental.inventory_id = inventory.inventory_id
+GROUP BY
+	film.film_id
+ORDER BY
+	count desc
+LIMIT
+	10
